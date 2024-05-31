392f0f422e59dc04eee107a01e088c0d160562a4
Diego Loaiza <dfloaiza@gmail.com>
2024-05-31 14:59:10 -0500

    Add files via upload

---
+++ AThreeJsAndReactGame/src/components/Ball.jsx
@@ -0,0 +1,21 @@
+import { useGLTF } from '@react-three/drei'
+
+const Ball = (position) =>
+    {
+        const {nodes , materials } = useGLTF("/models/basketball.glb");
+
+        return(
+            <mesh 
+                position={position} 
+                geometry={nodes.Sphere.geometry}
+                material={materials["Material.001"]}
+                castShadow
+                receiveShadow
+             />
+        );
+
+    };
+
+useGLTF.preload("/models/basketball.glb");
+
+export default Ball;
