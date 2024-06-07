f00b3d7753c99cee6b15d0a4c995ca55a25984ac
Diego Loaiza <dfloaiza@gmail.com>
2024-06-07 15:25:59 -0500

    Update Ball.jsx

--- AThreeJsAndReactGame/src/components/Ball.jsx
+++ AThreeJsAndReactGame/src/components/Ball.jsx
@@ -1,20 +1,19 @@
-import { useGLTF } from '@react-three/drei'
+import React from 'react';
+import { useGLTF } from "@react-three/drei";
 
-const Ball = (position) =>
-    {
-        const {nodes , materials } = useGLTF("/models/basketball.glb");
+const Ball = ({ position }) => {
+  const { nodes, materials } = useGLTF("/models/basketball.glb");
 
-        return(
-            <mesh 
-                position={position} 
-                geometry={nodes.Sphere.geometry}
-                material={materials["Material.001"]}
-                castShadow
-                receiveShadow
-             />
-        );
-
-    };
+  return (
+    <mesh
+      position={position}
+      geometry={nodes.Sphere.geometry}
+      material={materials["Material.001"]}
+      castShadow
+      receiveShadow
+    />
+  );
+};
 
 useGLTF.preload("/models/basketball.glb");
 
