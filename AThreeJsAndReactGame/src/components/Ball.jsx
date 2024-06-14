6cb5d115c3bea4253b22363b793d25e9f20e789b
Diego Loaiza <dfloaiza@gmail.com>
2024-06-14 14:49:20 -0500

    Update Ball.jsx

--- AThreeJsAndReactGame/src/components/Ball.jsx
+++ AThreeJsAndReactGame/src/components/Ball.jsx
@@ -1,20 +1,23 @@
 import React from 'react';
 import { useGLTF } from "@react-three/drei";
+import { RigidBody } from '@react-three/rapier';
 
 const Ball = ({ position }) => {
   const { nodes, materials } = useGLTF("/models/basketball.glb");
 
   return (
-    <mesh
-      position={position}
-      geometry={nodes.Sphere.geometry}
-      material={materials["Material.001"]}
-      castShadow
-      receiveShadow
-    />
+    <group position={position} scale={0.7}>
+      <RigidBody colliders="ball" restituion={1} friction={0.2}>
+        <mesh
+          position={position}
+          geometry={nodes.Sphere.geometry}
+          material={materials["Material.001"]}
+          castShadow
+          receiveShadow
+        />
+      </RigidBody>
+    </group>
   );
 };
-
 useGLTF.preload("/models/basketball.glb");
-
 export default Ball;
